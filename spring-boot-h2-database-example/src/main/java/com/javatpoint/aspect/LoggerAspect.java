0dcc9c0875053be78727fad6a3949059a8e9efb1
Andrew <andrewsselvaraj@gmail.com>
2022-10-22 00:06:17 -0700

    AOP NEW CODE

---
+++ spring-boot-h2-database-example/src/main/java/com/javatpoint/aspect/LoggerAspect.java
@@ -0,0 +1,29 @@
+package com.javatpoint.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.springframework.stereotype.Component;
+@Aspect  
+@Component  
+public class LoggerAspect {
+	@Before(value = "execution(* com.javatpoint.service.StudentService.*(..)) and args(id)")  
+	public void beforeAdvice(JoinPoint joinPoint,int id) {  
+	System.out.println("Method Called" + joinPoint.getSignature());  
+	//System.out.println("Creating Employee with first name - "+id);  
+	} 
+	
+	@After(value = "execution(* com.javatpoint.service.StudentService.*(..)) and args(id)")  
+	public void afterAdvice(JoinPoint joinPoint,int id) {  
+	System.out.println("Method Successfully implemented" + joinPoint.getSignature());  
+	//System.out.println("Creating Employee with first name - "+id);  
+	}
+	
+	@AfterThrowing(value = "execution(* com.javatpoint.service.StudentService.*(..)) and args(id)")  
+	public void afterThrowing(JoinPoint joinPoint,int id) {  
+	System.out.println("afterThrowing " + joinPoint.getSignature());  
+	//System.out.println("Creating Employee with first name - "+id);  
+	}
+}
