42881a7addaabfb2e335743c68d3dd34cae50af8
Andrew <andrewsselvaraj@gmail.com>
2022-10-27 23:15:25 -0700

    save all student

--- spring-boot-h2-database-example/src/main/java/com/javatpoint/service/StudentService.java
+++ spring-boot-h2-database-example/src/main/java/com/javatpoint/service/StudentService.java
@@ -18,6 +18,25 @@ List<Student> students = new ArrayList<Student>();
 studentRepository.findAll().forEach(student -> students.add(student));
 return students;
 }
+public List<Student> saveAllStudent() 
+{
+List<Student> students = new ArrayList<Student>();
+Student s = new Student();
+s.setId(987654321);
+s.setAge(100);
+s.setEmail("andrew_insi@yahoo.com");
+s.setName("name");
+students.add(s);
+
+Student s1 = new Student();
+s1.setId(987654321);
+s1.setAge(100);
+s1.setEmail("hey@yahoo.com");
+s1.setName("name2");
+students.add(s1);
+studentRepository.saveAll(students);
+return students;
+}
 //getting a specific record
 public Student getStudentById(int id) 
 {
