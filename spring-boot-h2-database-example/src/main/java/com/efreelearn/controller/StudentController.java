7ac55f0320668386729c365baf075f9fe12b49f9
DELL <DELL@DESKTOP-LF9C4GT>
2023-02-25 23:19:18 +0530

    consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE

--- spring-boot-h2-database-example/src/main/java/com/efreelearn/controller/StudentController.java
+++ spring-boot-h2-database-example/src/main/java/com/efreelearn/controller/StudentController.java
@@ -1,12 +1,14 @@
 package com.efreelearn.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.efreelearn.model.Student;
@@ -77,6 +79,15 @@ private int saveStudent(@RequestBody Student student)
 studentService.saveOrUpdate(student);
 return student.getId();
 }
+//@PostMapping("/student", consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(method = RequestMethod.POST, value = "/student1", consumes = MediaType.ALL_VALUE)
+private int saveStudent1(@RequestBody Student student) 
+{
+studentService.saveOrUpdate(student);
+return student.getId();
+}
+
+
 @PostMapping("/loginrequestbody")
 private Student loginRequestBody(@RequestBody Student student) 
 {
