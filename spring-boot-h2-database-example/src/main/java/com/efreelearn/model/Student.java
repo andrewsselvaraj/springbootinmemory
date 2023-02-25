56a94616b12e3e8ddf92f07f33d214496d46c575
DELL <DELL@DESKTOP-LF9C4GT>
2023-02-25 14:54:10 +0530

    spring 3.0 jakarta

--- spring-boot-h2-database-example/src/main/java/com/efreelearn/model/Student.java
+++ spring-boot-h2-database-example/src/main/java/com/efreelearn/model/Student.java
@@ -1,8 +1,10 @@
 package com.efreelearn.model;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+ 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 //mark class as an Entity 
 @Entity
 //defining class name as Table name
